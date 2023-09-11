84ffa919e474b75f9a308a6a54332f9dd375736d
farhan-ahmad-tufail <103448338+farhan-ahmad-tufail@users.noreply.github.com>
2023-09-11 12:18:33 +0000

    typeConversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let scores = 34
+
+console.log(typeof scores);  
+console.log(typeof(scores)); //both are correct ways to find type 
+
+
+let score = "34ac"
+
+let valueInNumber = Number(score)  //type conversion to number
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// "33" => 33 string to number 
+//"33abc"  => Nan - not a number
+// true = 1 and false =0
+
+
+let isLoggedIn = 1 
+
+// let isLoggedIn = "gar"
+
+let boolenaIsLogginIn = Boolean(isLoggedIn)
+console.log(boolenaIsLogginIn)
+
+
+
+
+
+
+
+
