0b5383b41e88b5f22aa21690effc1c7724e92927
farhan-ahmad-tufail <103448338+farhan-ahmad-tufail@users.noreply.github.com>
2023-09-11 12:41:42 +0000

    operations added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,34 +1,69 @@
-let scores = 34
+// let scores = 34
 
-console.log(typeof scores);  
-console.log(typeof(scores)); //both are correct ways to find type 
-
-
-let score = "34ac"
-
-let valueInNumber = Number(score)  //type conversion to number
-console.log(typeof valueInNumber);
-
-console.log(valueInNumber);
+// console.log(typeof scores);  
+// console.log(typeof(scores)); //both are correct ways to find type 
 
 
-// "33" => 33 string to number 
-//"33abc"  => Nan - not a number
-// true = 1 and false =0
+// let score = "34ac"
+
+// let valueInNumber = Number(score)  //type conversion to number
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
 
 
-let isLoggedIn = 1 
-
-// let isLoggedIn = "gar"
-
-let boolenaIsLogginIn = Boolean(isLoggedIn)
-console.log(boolenaIsLogginIn)
+// // "33" => 33 string to number 
+// //"33abc"  => Nan - not a number
+// // true = 1 and false =0
 
 
+// let isLoggedIn = 1 
 
-let num = 44
-let strNumber = String(num)
-console.log(typeof strNumber);
+// // let isLoggedIn = "gar"
+
+// let boolenaIsLogginIn = Boolean(isLoggedIn)
+// console.log(boolenaIsLogginIn)
+
+
+
+// let num = 44
+// let strNumber = String(num)
+// console.log(typeof strNumber);
+
+
+
+
+// ******************************* Operations *************************************
+
+
+
+let value = 4
+let negValue = -value
+
+console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**4) //2 power 4 = 16
+console.log(2%2)
+console.log(2/2)
+
+
+
+
+let str1 = "Hello"
+let str2 = " Farhan"
+
+let str3 = str1 + str2
+console.log (str3);
+
+console.log("1" + 2);  //bad practice
+console.log( 1 + "2");  //bad practice
+console.log( "1" + 3 + 3);  //bad practice //yahan par add nhi hua
+console.log( 1 + 3 +"2");  //bad practice  //yahan par add ho gya starting me
+
+// console.log(+true);  //bad practice
+
 
 
 
