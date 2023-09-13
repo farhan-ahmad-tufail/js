067e1afbd9f7d8572ee6754e18dd22b9a8a6b9df
FARHAN AHMAD TUFAIL <103448338+farhan-ahmad-tufail@users.noreply.github.com>
2023-09-13 14:29:03 +0000

    arrays updated

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -9,20 +9,22 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+// const all_new_heros = [...marvel_heros, ...dc_heros] //common way and easiest way to concatenate
 
-// console.log(all_new_heros);
+// // console.log(all_new_heros);
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// const real_another_array = another_array.flat(Infinity) // all arrays in single array list
+// console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
+// console.log(Array.isArray("Hitesh")) //boolean ki array hai ki nhi
+console.log(Array.from("Hitesh")) //[ 'H', 'i', 't', 'e', 's', 'h' ]
 console.log(Array.from({name: "hitesh"})) // interesting
+
+
 
 let score1 = 100
 let score2 = 200
